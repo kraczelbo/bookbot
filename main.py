b7d88ac30005ee8afc5e0756b65b8e78e5d7be3c
kraczelbo <kraczelbo@gmail.com>
2023-10-27 07:33:14 -0500

    updated the main.py again.

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+def report(letters):
+    formatted_lines = []
+    for i, count in letters.items():
+        formatted_lines.append(f"The '{i}' character was found {count} times")
+    return "\n".join(formatted_lines)
+
+def count_letters(words):
+    letter_count = {}
+    text = words.replace(" ", "").lower()
+    for char in text:
+        if char.isalpha():
+            if char in letter_count:
+                letter_count[char] += 1
+            else:
+                letter_count[char] = 1
+
+    sorted_counts = dict(sorted(letter_count.items()))
+    return sorted_counts
+
 def count_words(book):
     words = book.split()
     for i in range(0, len(words)+1):
@@ -15,8 +34,11 @@ def main():
     book_path = ("books/frankenstein.txt")
     read = read_book(book_path)
     count = count_words(read)
+    letter_count = count_letters(read)
+    letter_list = report(letter_count)
     print(read)
-    print(count)
+    print(f"{count} words found in the document")
+    print(letter_list)
     
 
 if __name__ == "__main__":
