e2b4ceb2de85d235721e851bc56784c2a72caff7
kraczelbo <kraczelbo@gmail.com>
2025-08-04 20:10:14 -0500

    changed the import stats line to a wild card value

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from stats import (
-    get_word_count,
-    get_book_text,
-    count_characters)
+from stats import *
 import sys
 if len(sys.argv) != 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
 argument1 = sys.argv[1]
-#argument2 = sys.argv[2]
-
-    
-
-
 
 def main():
     book_path = argument1
     read = get_book_text(book_path)
     word_count = get_word_count(read)
     characters = count_characters(read)
-
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
